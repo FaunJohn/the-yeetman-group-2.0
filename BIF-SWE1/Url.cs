dd4f6517a5f4c967091733b5de8339b5ea0e9e0b
bonig <michaelbonigl@gmx.net>
2019-10-17 14:38:14 +0200

    yeet

---
+++ BIF-SWE1/Url.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BIF.SWE1.Interfaces;
+
+namespace BIF_SWE1
+{
+    class Url : IUrl
+    {
+        public Url(string path)
+        {
+            Path = path;
+            RawUrl = path;
+        }
+
+        public string RawUrl { get; private set; }
+
+        public string Path { get; private set; }
+
+        public IDictionary<string, string> Parameter => throw new NotImplementedException();
+
+        public int ParameterCount => throw new NotImplementedException();
+
+        public string[] Segments => throw new NotImplementedException();
+
+        public string FileName => throw new NotImplementedException();
+
+        public string Extension => throw new NotImplementedException();
+
+        public string Fragment => throw new NotImplementedException();
+    }
+}
