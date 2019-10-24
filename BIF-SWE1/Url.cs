b548ce98d281f2cea332a4f84ea244abeef1fe8b
FaunJohn <jgc.bauer@gmail.com>
2019-10-24 12:13:25 +0200

    git gud

--- BIF-SWE1/Url.cs
+++ BIF-SWE1/Url.cs
@@ -11,13 +11,39 @@ namespace BIF_SWE1
         {
             Path = path;
             RawUrl = path;
+
+            if (RawUrl == null)
+            {
+                RawUrl = "/";
+            }
+
+            if (Path != null)
+            {
+                string[] temp = Path.Split("&");
+                string[] tempVarTwo = temp[1].Split("?");
+
+                Parameter = new Dictionary<string, string>();
+
+                if (temp.Length >= 2)
+                {
+                    string[] par = temp[1].Split("=");
+                    Parameter[par[0]] = par[1];
+                    if(tempVarTwo.Length >= 2)
+                    {
+                        string[] parTwo = tempVarTwo[1].Split("=");
+                        Parameter[parTwo[0]] = parTwo[1];
+                    }
+                }
+            }
+
         }
 
         public string RawUrl { get; private set; }
 
         public string Path { get; private set; }
 
-        public IDictionary<string, string> Parameter => throw new NotImplementedException();
+        public IDictionary<string, string> Parameter { get; private set; }
+
 
         public int ParameterCount => throw new NotImplementedException();
 
